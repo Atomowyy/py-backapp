e6a2dde8aa6dd4ca84316f07b9b9540212ac2225
Wojciech Kowal <w.kowal202@gmail.com>
2024-10-09 20:47:15 +0200

    Add basic message sending to server

--- backapp.py
+++ backapp.py
@@ -1,2 +1,16 @@
+import socket
 
-print('Hello There!')
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to server
+client_socket.connect(('localhost', 1234))
+
+try:
+    message = 'Hello There 12345!'
+    client_socket.sendall(message.encode())
+
+except socket.error as err:
+    print(f'Socket error: {err}')
+
+finally:
+    client_socket.close()
