5cec677b942dce3d3f8414c50fb7e6951fe09b31
Wojciech Kowal <w.kowal202@gmail.com>
2024-10-10 09:00:34 +0200

    Secure client communication with TLS

--- backapp.py
+++ backapp.py
@@ -1,16 +1,29 @@
 import socket
+import ssl
 
+# create client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# create ssl context
+context = ssl.create_default_context()
+
+# disable certificate verification (required for self-signed certificates to work properly)
+# FIXME:
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
+
+# wrap client socket with ssl
+secure_socket = context.wrap_socket(client_socket, server_hostname='localhost')
+
 # connect to server
-client_socket.connect(('localhost', 1234))
+secure_socket.connect(('localhost', 1234))
 
 try:
     message = 'Hello There 12345!'
-    client_socket.sendall(message.encode())
+    secure_socket.sendall(message.encode())  # sendall - wait until all data is sent, else error
 
 except socket.error as err:
     print(f'Socket error: {err}')
 
 finally:
-    client_socket.close()
+    secure_socket.close()
