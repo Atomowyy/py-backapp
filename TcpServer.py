c9926b825aa00887e03e3877cbd0ebc43c7668db
Wojciech Kowal <w.kowal202@gmail.com>
2024-10-10 08:59:07 +0200

    Secure server communication with TLS

--- TcpServer.py
+++ TcpServer.py
@@ -1,14 +1,27 @@
 import socket
+import ssl
+
+import os
+
 
 class TcpServer:
     @classmethod
     def get_host_ip(cls):
         return socket.gethostbyname(socket.gethostname())
 
-    def __init__(self, port, backlog, tcp_buffer_size):
+    def __init__(self, certfile, keyfile, port=1234, backlog=0, tcp_buffer_size=32_768):
         self.port = port
         self.backlog = backlog
         self.tcp_buffer_size = tcp_buffer_size
+
+        # check if cert and key file exist
+        if (not os.path.isfile(certfile)) or (not os.path.isfile(keyfile)):
+            raise Exception("Certfile and keyfile must be provided")
+
+        self.certfile = certfile
+        self.keyfile = keyfile
+
+        self.context = None
         self.server_socket = None
 
     def run(self):
@@ -22,21 +35,32 @@ class TcpServer:
         # start listening and set backlog
         self.server_socket.listen(self.backlog)
 
+        # wrapp the socket with SSL
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.context.minimum_version = ssl.TLSVersion.TLSv1_2  # set minimum TLS version to 1.2
+        # load cert and key files
+        self.context.load_cert_chain(certfile=self.certfile, keyfile=self.keyfile)
+
         # waiting for connections
         print(f'Started py-backapp server on {self.get_host_ip()}:{self.port}')
         while True:
             try:
-                connection, client_address = self.server_socket.accept()
-                print(f'Accepted connection from {client_address[0]}:{client_address[1]}')
-                self.handle_connection(connection)
+                client_socket, client_address = self.server_socket.accept()
+                self.handle_connection(client_socket, client_address)
             except socket.error as err:
                 print(f'Socket error: {err}')
 
-    def handle_connection(self, connection):
+    def handle_connection(self, client_socket, client_address):
+        # secure client socket with ssl
+        secure_socket = self.context.wrap_socket(client_socket, server_side=True)
+
+        # print connection info
+        print(f'Accepted connection from {client_address[0]}:{client_address[1]}, TLS version: {secure_socket.version()}')
+
         try:
             # receive data
             while True:
-                data = connection.recv(self.tcp_buffer_size)
+                data = secure_socket.recv(self.tcp_buffer_size)
 
                 if data:
                     print(f'Received: {data.decode()}')
@@ -47,4 +71,4 @@ class TcpServer:
             print(f'Socket error: {err}')
 
         finally:
-            connection.close()
+            secure_socket.close()
--- server.py
+++ server.py
@@ -4,9 +4,13 @@ PORT = 1234
 BACKLOG = 0  # number of allowed connections in the queue
 TCP_BUFFER_SIZE = 32_768  # buffer size in bytes, max = 65_536
 
+# FIXME: Use different certificates these are for development purposes only !!!
+CERTFILE = 'certificates/py-backapp-development.crt'
+KEYFILE = 'certificates/py-backapp-development.key'
+
 try:
     # initialize server
-    server = TcpServer(PORT, BACKLOG, TCP_BUFFER_SIZE)
+    server = TcpServer(CERTFILE, KEYFILE, PORT, BACKLOG, TCP_BUFFER_SIZE)
 
     # run the server
     server.run()
