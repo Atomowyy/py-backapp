4b8342d38899fc263bbd8717a4f840a2680357f8
Wojciech Kowal <w.kowal202@gmail.com>
2024-10-30 18:06:35 +0100

    Implement TcpClient class

--- client/backapp.py
+++ client/backapp.py
@@ -1,175 +1,32 @@
-import socket
-import ssl
+from tcp_client import TcpClient
 
-import tarfile
-
-from server.tcp_server import ServerActions
-
-import os
 from datetime import datetime
+import os
 
-
-# create client socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# create ssl context
-context = ssl.create_default_context()
-
-# disable certificate verification (required for self-signed certificates to work properly)
-# FIXME:
-context.check_hostname = False
-context.verify_mode = ssl.CERT_NONE
-
-# wrap client socket with ssl
-secure_socket = context.wrap_socket(client_socket, server_hostname='localhost')
-
-# connect to server
-secure_socket.connect(('localhost', 1234))
+client = TcpClient()
 
 #####################################
-# print('\nGet auth token')
-# command = ServerActions.get_token('admin', 'dupa')
-# secure_socket.sendall(command)
-#
-# token = secure_socket.recv(1024).decode()
-# print(f'Auth token: {token}')
-
+# client.get_auth_token()  # 0 - token received, 1 - error
 #####################################
-# send token to verify yourself (do this before every request, except for Getting auth token)
-token = ''
-auth = ServerActions.authenticate('admin', token)
-secure_socket.send(auth)
+token_is_valid = (client.verify_token() == 0)  # 0 - valid, 1 - invalid
 
-auth_response = secure_socket.recv(1024).decode()
-print(f'Auth response: {auth_response}')
-
-if auth_response == 'Authentication failed':
-    raise Exception('Authentication failed')
-
+if not token_is_valid:
+    raise Exception("Invalid token")
 #####################################
-# print('\nTesting file transfer')
-# command = ServerActions.store('test/test2/')
-# secure_socket.send(command)  # sendall - wait until all data is sent, else error
-#
-# path = '../LICENSE'
-# socket_file = secure_socket.makefile('wb')
-# with tarfile.open(fileobj=socket_file, mode='w|') as socket_tar:
-#     socket_tar.add(path, arcname=os.path.basename(path))
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
 
-#####################################
-# print('\nTesting folder transfer')
-# command = ServerActions.store('test/folder')
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# path = '../screenshots'  # send directory
-# # path = '../screenshots/'  # send only contents of directory
-# with tarfile.open(fileobj=secure_socket.makefile('wb'), mode='w|') as tar:
-#     tar.add(path, arcname=os.path.basename(path))
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
+response = client.store('../LICENSE', 'test/test2/')  # file transfer
+# response = client.store('../screenshots', 'test/folder')  # dir transfer
 
-#####################################
-# print('\nTesting listing')
-# command = ServerActions.list_data('/test/folder/screenshots')
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get server response - listing of files
-# ls = secure_socket.recv(1024)
-# print(ls.decode())
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
+# response = client.get('/test/test2/LICENSE', './')  # get file
+# response = client.get('/test', './', True)  # get dir
 
-#####################################
-# print('\nTesting getting modification date')
-# command = ServerActions.get_modification_date('/test/folder/screenshots/docker_1.png')
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # print local modification date
-# print(f'modification date: {datetime.fromtimestamp(os.path.getmtime("../screenshots/docker_1.png"))}')
-#
-# # get modification date
-# response = secure_socket.recv(1024)
-# modification_date = datetime.fromisoformat(response.decode())
-# print(f'Modification date on server: {modification_date}')
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
+# ls, response = client.list_data('/test/folder/screenshots')  # list data
+# print(ls)
 
-#####################################
-# print('\nTesting Deleting')
-# command = ServerActions.delete('/')
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
+# print(f'local modification date: {datetime.fromtimestamp(os.path.getmtime("../screenshots/docker_1.png"))}')
+# mod_date, response = client.get_modification_date('/test/folder/screenshots/docker_1.png')  # get modification date
+# print(mod_date)
 
-#####################################
-# print('\nTesting file retrieving')
-# command = ServerActions.get_file('/test/test2/LICENSE')
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get and extract tar archive
-# socket_file = secure_socket.makefile('rb')
-# with tarfile.open(fileobj=socket_file, mode='r|') as tar:
-#     tar.extractall('./', filter='tar')
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
+# response = client.delete('/')  # delete all data on the server
 
-#####################################
-# print('\nTesting directory retrieving')
-# command = ServerActions.get_dir('/test')
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get and extract tar archive
-# socket_file = secure_socket.makefile('rb')
-#
-# with tarfile.open(fileobj=socket_file, mode='r|') as tar:
-#     tar.extractall('./', filter='tar')
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
-
-#####################################
-# print('\nTest sending invalid action')
-# command = 'I AM INVALID   1234'.encode()
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
-
-#####################################
-# print('\nTest sending invalid action #2')
-#
-# # get binary data from .img
-# with open('../screenshots/docker_1.png', 'rb') as tmp:
-#     data = tmp.read(1024)
-#     command = data
-#
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
-
-#####################################
-# print('\nTest sending invalid path')
-# command = ServerActions.get_file('../')  # move outside client's 'data' directory
-#
-# secure_socket.sendall(command)  # sendall - wait until all data is sent, else error
-#
-# # get server response
-# response = secure_socket.recv(1024)
-# print(response.decode())
+print(response)
---
+++ client/tcp_client.py
@@ -0,0 +1,164 @@
+import socket
+import ssl
+import json
+
+import tarfile
+import os
+
+from datetime import datetime
+
+
+class TcpClient:
+    # load config from config.json
+    config = json.load(open('config.json', 'r'))
+
+    keys_to_check = {'server', 'port', 'username', 'token'}
+    if not keys_to_check.issubset(config.keys()):
+        raise Exception('Invalid client config file')
+
+    server = config['server']
+    port = int(config['port'])
+    username = config['username']
+    token = config['token']
+
+    def __init__(self, verify_certificate: bool = False):
+        # create client socket
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # create ssl context
+        context = ssl.create_default_context()
+
+        # disable certificate verification (required for self-signed certificates to work properly)
+        if verify_certificate is False:
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
+
+        # wrap client socket with ssl
+        self.secure_socket = context.wrap_socket(client_socket)
+
+        # connect to the server
+        self.secure_socket.connect((self.server, self.port))
+
+    def _send_command(self, command: bytes) -> None:
+        self.secure_socket.sendall(command)
+
+    def _get_response(self) -> str:
+        return self.secure_socket.recv(1024).decode()
+
+    def send_auth_token(self) -> str:
+        self._send_command(ServerActions.authenticate(self.username, self.token))
+        return self._get_response()
+
+    def verify_token(self) -> int:
+        response = self.send_auth_token()
+
+        if response == 'Authentication failed':
+            self.secure_socket.close()
+            return -1
+
+        return 0
+
+    def get_auth_token(self) -> int:
+        password = input(f'Password for {self.config["username"]}: ')
+        self._send_command(ServerActions.get_token(self.username, password))
+        response = self._get_response()
+
+        if response == 'Authentication failed':
+            return -1
+
+        print('Token received')
+        # if authentication was successful -> you get a token
+        self.config['token'] = response
+
+        # dump config to config.json
+        with open('config.json', 'w') as user_db:
+            user_db.write(json.dumps(self.config, indent='\t'))
+
+        return 0
+
+    def store(self, path, server_path) -> str:
+        self._send_command(ServerActions.store(server_path))
+
+        socket_file = self.secure_socket.makefile(mode='wb')
+        with tarfile.open(fileobj=socket_file, mode='w|') as socket_tar:
+            socket_tar.add(path, arcname=os.path.basename(path))
+        socket_file.flush()
+        socket_file.close()
+
+        return self._get_response()
+
+    def get(self, server_path, extract_path, is_dir: bool = False) -> str:
+        if is_dir:
+            self._send_command(ServerActions.get_dir(server_path))
+        else:
+            self._send_command(ServerActions.get_file(server_path))
+
+        socket_file = self.secure_socket.makefile('rb')
+        with tarfile.open(fileobj=socket_file, mode='r|') as tar:
+            tar.extractall(extract_path, filter='tar')
+
+        return self._get_response()
+
+    def get_dir(self, server_path, extract_path) -> str:
+        self._send_command(ServerActions.get_dir(server_path))
+
+        socket_file = self.secure_socket.makefile('rb')
+        with tarfile.open(fileobj=socket_file, mode='r|') as tar:
+            tar.extractall(extract_path, filter='tar')
+
+        return self._get_response()
+
+    def list_data(self, server_path) -> tuple[str, str]:
+        self._send_command(ServerActions.list_data(server_path))
+
+        listing = self._get_response()
+        response = self._get_response()
+        return listing, response
+
+    def get_modification_date(self, server_path) -> tuple[datetime, str]:
+        self._send_command(ServerActions.get_modification_date(server_path))
+
+        modification_date = datetime.fromisoformat(self._get_response())
+        response = self._get_response()
+        return modification_date, response
+
+    def delete(self, server_path) -> str:
+        self._send_command(ServerActions.delete(server_path))
+
+        return self._get_response()
+
+
+class ServerActions:
+    spacer = '<;;;>'
+
+    @classmethod
+    def get_token(cls, user: str, password: str) -> bytes:
+        return ('GET TOKEN' + cls.spacer + user + cls.spacer + password).encode()
+
+    @classmethod
+    def authenticate(cls, user: str, token: str) -> bytes:
+        return ('AUTHENTICATE' + cls.spacer + user + cls.spacer + token).encode()
+
+    @classmethod
+    def store(cls, path: str) -> bytes:
+        return ('STORE' + cls.spacer + path).encode()
+
+    @classmethod
+    def get_file(cls, file_path: str) -> bytes:
+        return ('GET FILE' + cls.spacer + file_path).encode()
+
+    @classmethod
+    def get_dir(cls, dir_path: str) -> bytes:
+        return ('GET DIR' + cls.spacer + dir_path).encode()
+
+    @classmethod
+    def get_modification_date(cls, path: str) -> bytes:
+        return ('GET MODIFICATION DATE' + cls.spacer + path).encode()
+
+    @classmethod
+    def delete(cls, path: str) -> bytes:
+        return ('DELETE DATA' + cls.spacer + path).encode()
+
+    @classmethod
+    def list_data(cls, path: str) -> bytes:
+        return ('LIST DATA' + cls.spacer + path).encode()
