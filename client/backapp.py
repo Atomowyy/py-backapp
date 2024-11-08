14ef552af73e6143fc60f190951cf0a18e6997a4
Wojciech Kowal <w.kowal202@gmail.com>
2024-11-08 18:33:25 +0100

    Refactor and Implement directory synchronization

--- client/backapp.py
+++ client/backapp.py
@@ -1,11 +1,7 @@
-from platform import system
-from socket import socket, error as socket_error
-from time import sleep
-from xml.sax import parse
+from socket import error as socket_error
 from tcp_client import TcpClient
-from datetime import datetime
+from datetime import datetime, UTC
 import os
-import sys
 import argparse
 import json
 
@@ -25,22 +21,6 @@ def server_check(config: json) -> bool:
     return True
 
 
-# def token_check(config, client) -> bool:
-#     token: str = config['token']
-#     if token == '':
-#         server_response = client.get_auth_token()
-#         #print(server_response)
-#         while server_response == -1:
-#             client = TcpClient()
-#             #print("Authentication failure")
-#             server_response = client.get_auth_token()
-#             #print(server_response)
-#         print("Authenticated successfully!")
-#         #print('Token granted, please restart py-backapp')
-#         return True
-#     return False
-
-
 def server_set(config) -> None:
     config['server'] = str(input('Server IP: '))
     config['port'] = str(input('Server port: '))
@@ -58,10 +38,10 @@ def user_set(config) -> None:
 def print_menu() -> None:
     print('1.Send files to the server')
     print('2.Send whole directories to the server')
-    print('3.Download files from the server')
-    print('4.Download directories from the server')
-    print('5.List your uploaded files')
-    print('6.Synchronize data')
+    print('3.Download files/directories from the server')
+    print('4.List your uploaded files')
+    print('5.Synchronize file')
+    print('6.Synchronize directory')
     print('7.Delete selected uploaded files')
     print('8.Delete all of your uploaded files')
     print('9.Exit')
@@ -104,74 +84,146 @@ def menu() -> None:
         match input():
             case '1':  # dziala - wysyłanie pliku
                 print('--------------------------------------------')
-                path: str = input('Specify path to the file: ')
-                response = client.store(path, f'{config['username']}')
+                local_path: str = input('Specify path to the file: ')
+                response = client.store(local_path, f'{config['username']}')
                 print(response)
                 print('--------------------------------------------')
             case '2':  # dziala - wysyłanie katalogu
                 print('--------------------------------------------')
-                path: str = input('Specify path to the directory: ')
-                response = client.store(path, f'{config['username']}')
+                local_path: str = input('Specify path to the directory: ')
+                response = client.store(local_path, f'{config['username']}')
                 print(response)
                 print('--------------------------------------------')
-            case '3':  # - pobieranie pliku - działa, do poprawy, dodać try
+            case '3':  # - pobieranie pliku/folderu - działa, do poprawy, dodać try
                 print('--------------------------------------------')
                 filename: str = input(
-                    'Specify path to the file that you want to download: ')
+                    'Specify path to the file/directory that you want to download: ')
                 response = client.get(f'/{config['username']}/{filename}', './Downloads')
                 print(response)
                 print('--------------------------------------------')
-            case '4':  # pobieranie katalogu - dodać try
-                print('--------------------------------------------')
-                dirname: str = input(
-                    'Specify path to the directory that you want to download: ')
-                response = client.get(f'/{config['username']}/{dirname}', './Downloads', True)
-                print(response)
-                print('--------------------------------------------')
-            case '5':  # dziala
+            case '4':  # dziala
                 print('list your uploaded files')
                 ls, response = client.list_data(f'/{config['username']}')  # list data
                 print(ls)
-            case '6':
-                path = input('Specify path to the file/directory that you want to synchronise: ')
-                filename = path.split('/')[-1]
-                local_path = path.replace(filename, '')
-                ls, response = client.list_data(f'/{config['username']}')  # list data
+            case '5':
+                local_path = os.path.abspath(
+                    './' + input('Specify local path to the file that you want to synchronise: '))
+                remote_path = input('Specify remote path to the file that you want to synchronise: ')
+
+                if not os.path.isfile(local_path):
+                    print('File does not exist')
+                    client.close_connection()
+                    continue
+
+                filename = os.path.basename(local_path)
+                local_mod_date = datetime.fromtimestamp(os.path.getmtime(local_path), UTC)
+
+                ls_, response = client.list_data(
+                    f'/{config['username']}/{remote_path.replace(os.path.basename(remote_path), '')}'
+                )
+
+                if response == 'Invalid path':
+                    print('Invalid remote path')
+                    continue
+
+                ls = ls_.split('  ')
+
                 if filename in ls:
                     print(
-                        f'Local modification date: {datetime.fromtimestamp(os.path.getmtime(path))}')
+                        f'Local modification date: {local_mod_date}')
                     client = TcpClient()
                     client.send_auth_token()
-                    mod_date, response = client.get_modification_date(f'/{config['username']}/{filename}')
-                    print(f'Remote modification date: {mod_date}')
+                    remote_mod_date, response = client.get_modification_date(f'/{config['username']}/{filename}')
+                    print(f'Remote modification date: {remote_mod_date}')
 
-                    if datetime.fromtimestamp(os.path.getmtime(path)) == mod_date:
+                    if local_mod_date == remote_mod_date:
                         print('Files are up to date!')
-                    elif datetime.fromtimestamp(os.path.getmtime(path)) > mod_date:
+                    elif local_mod_date > remote_mod_date:
                         print('Remote file is older than local')
-                        decision = input('Do you want to upload current version to the server? [y]: ')
-                        if decision.upper() == 'Y':
+                        decision = input('Do you want to upload current version to the server? [n/y]: ')
+                        if decision.upper() in ('Y', 'YES'):
                             print('Sending files to the server...')
                             client = TcpClient()
                             client.send_auth_token()
-                            response = client.store(path, f'{config['username']}')
+                            response = client.store(
+                                local_path,
+                                f'{config['username']}/{remote_path.replace(os.path.basename(remote_path), '')}'
+                            )
                             print(response)
-                    elif datetime.fromtimestamp(os.path.getmtime(path)) < mod_date:
+                    elif local_mod_date < remote_mod_date:
                         print('Local file is older than remote')
-                        decision = input('Do you want to download current version from the server? [y]: ')
-                        if decision.upper() == 'Y':
+                        decision = input('Do you want to download current version from the server? [n/y]: ')
+                        print(decision.upper())
+                        if decision.upper() in ('Y', 'YES'):
                             print('Downloading files from the server...')
                             client = TcpClient()
                             client.send_auth_token()
-                            response = client.get(f'/{config['username']}/{filename}', local_path)
+                            response = client.get(
+                                f'/{config['username']}/{remote_path}',
+                                local_path.replace(os.path.basename(local_path), '')
+                            )
                             print(response)
                 else:
                     print('Specified file/directory was not found on the server')
-                exit()
+
+            case '6':
+                local_path = input('Specify local path to the directory that you want to synchronise: ').rstrip('/')
+                remote_path = input('Specify remote path to the directory that you want to synchronise: ')
+
+                if not os.path.isdir(local_path):
+                    print('Directory does not exist')
+                    client.close_connection()
+                    continue
+
+                ls_, response = client.list_data(f'/{config['username']}/{remote_path}')
+
+                if response == 'Invalid path':
+                    print('Invalid remote path')
+                    continue
+
+                local_ls = os.listdir(local_path)
+                remote_ls = ls_.split('  ')
+
+                files_dirs_to_compare = local_ls + remote_ls
+                # store/get missing items
+                for item in local_ls + remote_ls:
+                    if item not in remote_ls:
+                        files_dirs_to_compare.remove(item)
+
+                        client = TcpClient()
+                        client.send_auth_token()
+                        client.store(f'{local_path}/{item}', f'{config['username']}/{remote_path}')
+                    elif item not in local_ls:
+                        files_dirs_to_compare.remove(item)
+
+                        client = TcpClient()
+                        client.send_auth_token()
+                        client.get(f'{config['username']}/{remote_path}/{item}', f'{local_path}')
+
+                # compare modification dates
+                for item in files_dirs_to_compare:
+                    client = TcpClient()
+                    client.send_auth_token()
+
+                    local_mod_date = datetime.fromtimestamp(os.path.getmtime(local_path), UTC)
+                    remote_mod_date, _ = client.get_modification_date(f'{config['username']}/{remote_path}/{item}')
+
+                    if local_mod_date == remote_mod_date:
+                        continue
+                    elif local_mod_date > remote_mod_date:
+                        client = TcpClient()
+                        client.send_auth_token()
+                        client.store(f'{local_path}/{item}', f'{config['username']}/{remote_path}')
+                    elif local_mod_date < remote_mod_date:
+                        client = TcpClient()
+                        client.send_auth_token()
+                        client.get(f'{config['username']}/{remote_path}/{item}', f'{local_path}')
+
+                print('Directory synchronized successfully')
             case '7':  # usuwanie
                 print('--------------------------------------------')
-                path: str = input('Specify path to the file or folder that you want to delete: ')
-                response = client.delete(f'/{config['username']}/{path}')
+                local_path: str = input('Specify path to the file or folder that you want to delete: ')
+                response = client.delete(f'/{config['username']}/{local_path}')
                 print(response)
                 print('--------------------------------------------')
             case '8':
