ec72665b888ff89d6276a2c1828b7eea089ac3c9
Wojciech Lewko <wojciech.lewkoo@gmail.com>
2024-11-10 21:23:07 +0100

    CLI args completed

--- client/backapp.py
+++ client/backapp.py
@@ -264,9 +264,163 @@ def cli(args) -> None:
         exit()
     if args.password:
         client.get_auth_token()
+        exit()
+    client.send_auth_token()
+    if args.sendfile:
+        local_path: str = args.sendfile
+        response = client.store(local_path, f'{config['username']}')
+        print(response)
+        exit()
+    if args.senddir:
+        local_path: str = args.senddir
+        response = client.store(local_path, f'{config['username']}')
+        print(response)
+        exit()
+    if args.download:
+        filename: str = args.download
+        response = client.get(f'/{config['username']}/{filename}', './Downloads')
+        print(response)
+        exit()
+    if args.list:
+        ls, response = client.list_data(f'/{config['username']}')  # list data
+        print(ls)
+        exit()
+    if args.removefile:
+        local_path: str = args.removefile
+        response = client.delete(f'/{config['username']}/{local_path}')
+        print(response)
+    if args.removeall:
+        response = client.delete(f'/{config['username']}/')
+        print(response)
+    if args.synchronizefile:
+        if not args.localpath:
+            print('specify local path using -P <path>')
+            exit()
+        if not args.remotepath:
+            print('specify remote path using -z <path>')
+            exit()
+        local_path = args.localpath
+        if not os.path.isfile(local_path):
+            print('File does not exist')
+            client.close_connection()
+            exit()
 
+        remote_path = args.remotepath
+
+        ls_, response = client.list_data(
+            f'/{config['username']}/{remote_path.replace(os.path.basename(remote_path), '')}'
+        )
+
+        if response == 'Invalid Path':
+            print('Specified file/directory was not found on the server')
+            exit()
+
+        filename = os.path.basename(local_path)
+        local_mod_date = datetime.fromtimestamp(os.path.getmtime(local_path), UTC)
+
+        ls = ls_.split('  ')
+
+        if filename in ls:
+            print(
+                f'Local modification date: {local_mod_date}')
+            client = TcpClient()
+            client.send_auth_token()
+            remote_mod_date, response = client.get_modification_date(
+                f'/{config['username']}/{remote_path}'
+            )
+            print(f'Remote modification date: {remote_mod_date}')
+
+            if local_mod_date == remote_mod_date:
+                print('Files are up to date!')
+            elif local_mod_date > remote_mod_date:
+                print('Remote file is older than local')
+                decision = input('Do you want to upload current version to the server? [n/y]: ')
+                if decision.upper() in ('Y', 'YES'):
+                    print('Sending files to the server...')
+                    client = TcpClient()
+                    client.send_auth_token()
+                    response = client.store(
+                        local_path,
+                        f'{config['username']}/{remote_path.replace(os.path.basename(remote_path), '')}'
+                    )
+                    print(response)
+            elif local_mod_date < remote_mod_date:
+                print('Local file is older than remote')
+                decision = input('Do you want to download current version from the server? [n/y]: ')
+                if decision.upper() in ('Y', 'YES'):
+                    print('Downloading files from the server...')
+                    client = TcpClient()
+                    client.send_auth_token()
+                    response = client.get(
+                        f'/{config['username']}/{remote_path}',
+                        local_path.replace(os.path.basename(local_path), '')
+                    )
+                    print(response)
+        else:
+            print('Specified file/directory was not found on the server')
+
+        exit()
+    if args.synchronizedir:
+        if not args.localpath:
+            print('specify local path using -P <path>')
+            exit()
+        if not args.remotepath:
+            print('specify remote path using -z <path>')
+            exit()
+        local_path = args.localpath
+        remote_path = args.remotepath
+
+        if not os.path.isdir(local_path):
+            print('Directory does not exist')
+            client.close_connection()
+            exit()
+
+        ls_, response = client.list_data(f'/{config['username']}/{remote_path}')
+
+        if response == 'Invalid Path':
+            print('Specified file/directory was not found on the server')
+            exit()
+
+        local_ls = os.listdir(local_path)
+        remote_ls = ls_.split('  ')
+
+        files_dirs_to_compare = local_ls + remote_ls
+        # store/get missing items
+        for item in local_ls + remote_ls:
+            if item not in remote_ls:
+                files_dirs_to_compare.remove(item)
+
+                client = TcpClient()
+                client.send_auth_token()
+                client.store(f'{local_path}/{item}', f'{config['username']}/{remote_path}')
+            elif item not in local_ls:
+                files_dirs_to_compare.remove(item)
+
+                client = TcpClient()
+                client.send_auth_token()
+                client.get(f'{config['username']}/{remote_path}/{item}', f'{local_path}')
+
+        # compare modification dates
+        for item in files_dirs_to_compare:
+            client = TcpClient()
+            client.send_auth_token()
+
+            local_mod_date = datetime.fromtimestamp(os.path.getmtime(local_path), UTC)
+            remote_mod_date, _ = client.get_modification_date(f'{config['username']}/{remote_path}/{item}')
+
+            if local_mod_date == remote_mod_date:
+                continue
+            elif local_mod_date > remote_mod_date:
+                client = TcpClient()
+                client.send_auth_token()
+                client.store(f'{local_path}/{item}', f'{config['username']}/{remote_path}')
+            elif local_mod_date < remote_mod_date:
+                client = TcpClient()
+                client.send_auth_token()
+                client.get(f'{config['username']}/{remote_path}/{item}', f'{local_path}')
+
+        exit()
     exit()
-
 
 # reading arg
 parser = argparse.ArgumentParser(
@@ -278,13 +432,15 @@ parser.add_argument('-u', '--username')
 parser.add_argument('-p', '--password', action='store_true')
 parser.add_argument('-v', '--verify', action='store_true')
 parser.add_argument('-f', '--sendfile')
-parser.add_argument('-F', '--downloadfile')
 parser.add_argument('-d', '--senddir')
-parser.add_argument('-D', '--downloaddir')
-parser.add_argument('-l', '--list')
-parser.add_argument('-s', '--synchronize')
+parser.add_argument('-D', '--download')
+parser.add_argument('-l', '--list', action='store_true')
+parser.add_argument('-s', '--synchronizefile', action='store_true')
+parser.add_argument('-S', '--synchronizedir', action='store_true')
+parser.add_argument('-P', '--localpath')
+parser.add_argument('-z', '--remotepath')
 parser.add_argument('-r', '--removefile')
-parser.add_argument('-R', '--removeall')
+parser.add_argument('-R', '--removeall', action='store_true')
 args = parser.parse_args()
 
 try:
