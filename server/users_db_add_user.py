1d20a418c0efa6277f95048468d951b516fb50f3
Wojciech Kowal <w.kowal202@gmail.com>
2024-11-15 20:26:46 +0100

    Implement hiding password input when creating new server users

--- server/users_db_add_user.py
+++ server/users_db_add_user.py
@@ -1,7 +1,57 @@
+#!/usr/bin/env python3
+
 import json
 import hashlib
 import os
+import sys
 from base64 import b64encode
+
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import termios
+
+
+def get_passwd_unix(prompt: str) -> str:
+    print(prompt)
+
+    fd = sys.stdin.fileno()  # get file descriptor associated with file-like object (stdin, stdout, stderr)
+    old_settings = termios.tcgetattr(fd)  # Get current terminal settings
+
+    # terminal settings - canonical mode, disable echo, keep interrupts
+    new_settings = termios.tcgetattr(fd)
+    new_settings[3] = new_settings[3] & ~(termios.ICANON | termios.ECHO)
+    try:
+        termios.tcsetattr(fd, termios.TCSADRAIN, new_settings)
+
+        passwd = ''
+        ch = sys.stdin.read(1)  # Read one byte
+
+        while ch != '\n':
+            passwd += ch
+            ch = sys.stdin.read(1)
+    finally:
+        # Restore terminal settings
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return passwd
+
+
+def get_passwd_windows(prompt: str) -> str:
+    print(prompt)
+
+    ch = msvcrt.getch()
+    if ch == b'\x03':  # ctrl + c
+        print('Exiting...')
+        exit()
+
+    passwd = ''
+    while ch != b'\r':
+        passwd += ch.decode(errors='replace')
+        ch = msvcrt.getch()
+
+    return passwd
+
 
 users = json.load(open('users_db.json', 'r'))
 
@@ -10,8 +60,9 @@ if username in users:
     print('Username already exists')
     exit()
 
-password = input('Password: ')
-retyped_password = input('Retype Password: ')
+password = get_passwd_windows('Password: ') if sys.platform == 'win32' else get_passwd_unix('Password: ')
+retyped_password = (
+    get_passwd_windows('Retype Password: ')) if sys.platform == 'win32' else get_passwd_unix('Retype Password: ')
 
 if password != retyped_password:
     print('Passwords do not match')
